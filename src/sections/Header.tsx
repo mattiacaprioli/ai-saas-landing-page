6f0316737e5fa015ec173b7b1718d401e51e564a
Mattia Caprioli <capriolimattia1994@gmail.com>
2024-10-27 17:11:59 +0100

    first commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import logoImage from "@/assets/images/sphereal-logo.svg?url";
+
+export const navItems = [
+  {
+    name: "Features",
+    href: "#features",
+  },
+  {
+    name: "Pricing",
+    href: "#pricing",
+  },
+  {
+    name: "Testimonials",
+    href: "#testimonials",
+  },
+];
+
+export const loginItems = [
+  {
+    buttonVariant: "tertiary",
+    name: "Login",
+    href: "#login",
+  },
+  {
+    buttonVariant: "primary",
+    name: "Sign Up",
+    href: "#sign-up",
+  },
+];
+
+export const Header = () => {
+  return (
+    <header className="border-b border-gray-200/20">
+      <div className="container">
+        <div className="h-18 flex justify-between items-center">
+          <div className="flex gap-4 items-center">
+            <div
+              className="size-10 bg-gray-200 bg-[conic-gradient(from_45deg,var(--color-violet-400),var(--color-fuchsia-400),var(--color-amber-300),var(--color-teal-300),var(--color-violet-400))]"
+              style={{
+                maskImage: `url(${logoImage.src})`,
+                maskSize: "contain",
+              }}
+            ></div>
+            <div className="font-extrabold text-2xl">sphereal.ai</div>
+          </div>
+          <div className="h-full">
+            <nav className="h-full">
+              {navItems.map(({ name, href }) => {
+                return (
+                  <a
+                    href={href}
+                    key={href}
+                    className="h-full px-10 relative font-body text-xs tracking-widest text-gray-400 uppercase inline-flex items-center before:content-[''] before:absolute before:bottom-0 before:h-2 before:w-px before:bg-gray-200/20 before:left-0 last:after:content-[''] after:absolute after:bottom-0 after:h-2 after:w-px after:bg-gray-200/20 after:right-0"
+                  >
+                    {name}
+                  </a>
+                );
+              })}
+            </nav>
+          </div>
+          <div className="flex items-center">
+            <button className="size-10 rounded-lg border-2 border-transparent [background:linear-gradient(var(--color-gray-950),var(--color-gray-950))_content-box,conic-gradient(from_45deg,var(--color-violet-400),var(--color-fuchsia-400),var(--color-amber-300),var(--color-teal-300),var(--color-violet-400))_border-box] relative">
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-4 h-0.5 bg-gray-100 -translate-y-1"></div>
+              </div>
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-4 h-0.5 bg-gray-100 translate-y-1"></div>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

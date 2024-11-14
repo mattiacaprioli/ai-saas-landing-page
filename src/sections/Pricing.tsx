2ed899e0b21a599f6b9aaff68782e55eb8b5fe6a
Mattia Caprioli <capriolimattia1994@gmail.com>
2024-11-14 10:19:10 +0100

    finish pricing section

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -2,6 +2,9 @@ import { Button } from "@/components/Button";
 import { SectionBorder } from "@/components/SectionBorder";
 import { SectionContent } from "@/components/SectionContent";
 import { twMerge } from "tailwind-merge";
+import { ButtonProps } from "@/components/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 
 export const pricingTiers = [
   {
@@ -9,6 +12,7 @@ export const pricingTiers = [
     description: "AI chatbot, personalized recommendations",
     price: "Free",
     buttonText: "Get Started",
+    buttonVariant: "secondary",
     features: [
       "Access to AI chatbot for natural language conversations",
       "Basic task automation for simple workflows",
@@ -22,6 +26,7 @@ export const pricingTiers = [
     description: "Advanced AI capabilities for enhanced productivity",
     price: 99,
     buttonText: "Upgrade to Premium",
+    buttonVariant: "secondary",
     features: [
       "All Basic features included",
       "Priority access to new AI features and updates",
@@ -47,38 +52,71 @@ export const pricingTiers = [
     color: "teal",
     className: "lg:py-12 lg:my-6",
   },
-];
+] satisfies {
+  title: string;
+  description: string;
+  price: number | string | null;
+  buttonText: string;
+  features: string[];
+  color: string;
+  buttonVariant?: ButtonProps["variant"];
+  className?: string;
+}[];
 
 export const Pricing = () => {
   return (
-    <section className="pb-[1000px]">
+    <section>
       <div className="container">
-        <SectionBorder>
+        <SectionBorder borderTop>
           <SectionContent>
-            <h2 className="text-3xl font-semibold text-center text-gray-200">
+            <h2 className="text-3xl md:text-4xl lg:text-5xl leading-tight font-semibold text-center text-gray-200">
               Flexible plans for every need
             </h2>
-            <div className="mt-12">
+            <div className="mt-12 flex flex-col lg:flex-row lg:items-start gap-8">
               {pricingTiers.map((tier) => (
-                <div key={tier.title} className="border border-[var(--color-border)] rounded-3xl px-6 py-12">
-                  <h3 
+                <div
+                  key={tier.title}
+                  className={twMerge(
+                    "border border-[var(--color-border)] rounded-3xl px-6 py-12 max-w-sm mx-auto flex-1",
+                    tier.className
+                  )}
+                >
+                  <h3
                     className={twMerge(
-                      "font-semibold text-4xl", 
+                      "font-semibold text-4xl",
                       tier.color === "violet" && "text-violet-400",
                       tier.color === "amber" && "text-amber-300",
-                      tier.color === "teal" && "text-teal-400",
+                      tier.color === "teal" && "text-teal-400"
                     )}
                   >
                     {tier.title}
                   </h3>
                   <p className="mt-4 text-gray-400">{tier.description}</p>
-                  <div>
-                    <span className="text-7xl font-semibold text-gray-200">{tier.price}</span>
+                  <div className="mt-8">
+                    {typeof tier.price === "number" && (
+                      <span className="text-2xl font-semibold text-gray-200 align-top">
+                        $
+                      </span>
+                    )}
+                    <span className="text-7xl font-semibold text-gray-200">
+                      {tier.price ? tier.price : <>&nbsp;</>}
+                    </span>
                   </div>
-                  <Button>{tier.buttonText}</Button>
-                  <ul>
+                  <Button className="mt-8" block variant={tier.buttonVariant}>
+                    {tier.buttonText}
+                  </Button>
+                  <ul className="flex flex-col gap-4 mt-8">
                     {tier.features.map((feature) => (
-                      <li key={feature}>{feature}</li>
+                      <li
+                        key={feature}
+                        className="border-t border-[var(--color-border)] pt-4 flex gap-4"
+                      >
+                        <FontAwesomeIcon
+                          icon={faCheckCircle}
+                          className="size-6 text-violet-400 flex-shrink-0"
+                        />
+                        <span className="font-medium">{feature}</span>
+                      </li>
                     ))}
                   </ul>
                 </div>
